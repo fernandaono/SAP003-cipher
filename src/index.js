c098b8e0746c22be44c06f45a55b1cba2f38b893
fernanda <ono.frnd@gmail.com>
2019-08-21 15:40:35 -0300

    criada a function decode

--- src/index.js
+++ src/index.js
@@ -7,6 +7,18 @@ function codificar(){
         charCodes.push(str.charCodeAt(i)+nbr);
         //console.log(str.charCodeAt(i));
     }
-    let cifra = String.fromCharCode.apply(String,charCodes);
-    document.getElementById("cifra").innerHTML= cifra;
+        let cifra = String.fromCharCode.apply(String,charCodes);
+            document.getElementById("cifra").innerHTML= cifra;
+}
+
+function decodificar(){
+    let str = document.getElementById("mensagem").value;
+    let charCodes = [];
+    let nbr = parseInt(document.getElementById("deslocamento").value);
+
+    for (let i = 0; i < str.length; i++) {
+        charCodes.push(str.charCodeAt(i)-nbr);
+    }
+        let cifra = String.fromCharCode.apply(String,charCodes);
+            document.getElementById("cifra").innerHTML= cifra;
 }
